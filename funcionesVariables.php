556ade1efebbc40077c37e5194c73f7981b119a3
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2023-04-30 16:38:03 -0600

    Funciones variables

---
+++ funcionesVariables.php
@@ -0,0 +1,22 @@
+<?php
+
+function michi()
+{
+    echo "Meow!";
+}
+
+function dogo()
+{
+    echo "Woow!";
+}
+
+function zorro()
+{
+    echo "Zorro no te lo lleves!";
+}
+
+$function = readline("Escribe cual funcion quiere ejecutar \n");
+
+$function();
+
+echo "\n";
